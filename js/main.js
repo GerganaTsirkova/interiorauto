8963b41d7ede2837175e248aa3d8bbcf0c4efbfd
GerganaTsirkova <g_cirkova@mail.bg>
2019-04-30 14:45:51 +0300

    updated

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function startApp() {
+    function openMobileMenu() {
+        $('.close').css('display', 'block');
+        $('.nav').css('display', 'none');
+    }
+
+    function closeMobileMenu() {
+        $('.close').css('display', 'none');
+        $('.nav').css('display', 'block');
+    }
+}
