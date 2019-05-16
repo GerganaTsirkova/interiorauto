3e3992d2a8fb5e0719b2568d3e5d2aebf0edf67c
GerganaTsirkova <g_cirkova@mail.bg>
2019-05-16 20:01:24 +0300

    updated

--- docs/js/main.js
+++ docs/js/main.js
@@ -24,7 +24,7 @@ function start() {
     }
     let str = location.href.toLowerCase();
     $('nav.top-middle-menu > ul > li > a').removeClass('active');
-    if ((str === 'http://localhost:3000/') || (str === 'https://gerganatsirkova.github.io/slavi/') || (str === 'http://localhost:3000/en/') || (str === 'https://gerganatsirkova.github.io/slavi/en/') || (str === 'http://autorenovation.bg/') || (str === 'https://autorenovation.bg/en/')) {
+    if ((str === 'http://localhost:3000/') || (str === 'https://gerganatsirkova.github.io/slavi/') || (str === 'http://localhost:3000/en/') || (str === 'https://gerganatsirkova.github.io/slavi/en/') || (str === 'https://interiorauto.bg/') || (str === 'https://interiorauto.bg/en/')) {
         $('nav.top-middle-menu > ul > li:nth-child(1) > a').addClass('active');
     } else if (str.endsWith('/services/') || str.endsWith('/services-en/') || (str.indexOf('service') > 0)) {
         $('nav.top-middle-menu > ul > li:nth-child(2) > a').addClass('active');
